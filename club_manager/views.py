9de59a9891f0c18cdf26f7f62c522f05cf364a64
cmp0106 <cmp0106@DESKTOP-THNH2KU>
2021-07-05 18:17:03 -0400

    clubs.html template created

--- club_manager/views.py
+++ club_manager/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from club_manager.models import *
+from club_manager.models import Club
 
 
 def index(request):
@@ -8,5 +8,7 @@ def index(request):
 
 
 def clubs(request):
-    all_clubs = Club.objects.order_by('club_name')
-
+    """Show all clubs."""
+    clubs = Club.objects.order_by('club_name')
+    context = {'clubs': clubs}
+    return render(request, 'club_manager/clubs.html', context)
