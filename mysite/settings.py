532ab6d0eb24ba2fcb2914d9a8fae51955c836a7
cmp0106 <cmp0106@DESKTOP-THNH2KU>
2021-07-11 16:06:11 -0400

    debug set to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z)6v39cczb9%_77w6chsb9hk)4d3*zpnes3yrir3soxl9#m6#7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
